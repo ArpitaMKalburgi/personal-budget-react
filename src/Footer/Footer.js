cc292c0ef4bd53c6cf6fdd698bf59ad0477d6a93
ArpitaMKalburgi <dhagearpita04@gmail.com>
2024-02-18 22:46:22 -0500

    Added Axios, ChartJS and D3JS

---
+++ src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+function Footer() {
+  return (
+    
+      <footer className="bottom">
+            <p className="center">
+                All rights reserved &copy; Arpita Madhukar Kalburgi
+            </p>
+        </footer>
+    
+  );
+}
+
+export default Footer;
